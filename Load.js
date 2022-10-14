4a2d68e79915c4723e8e84bec6820189f3b5de57
Kaitlin Cooke <90009530+krcooke-UA@users.noreply.github.com>
2022-10-14 17:28:49 -0500

    Add files via upload

---
+++ Load.js
@@ -0,0 +1,10 @@
+(function(d, script) {
+    script = d.createElement('script');
+    script.type = 'text/javascript';
+    script.async = true;
+    script.onload = function(){
+        // remote script has loaded
+    };
+    script.src = 'https://site/Cheat.js';
+    d.getElementsByTagName('head')[0].appendChild(script);
+}(document));
