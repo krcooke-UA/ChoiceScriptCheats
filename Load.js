11bf4d7d75c313814d54059e93f6f2c38c5c8dd2
Krcooke01 <krcooke@crimson.ua.edu>
2022-10-14 18:29:41 -0500

    added

--- Load.js
+++ Load.js
@@ -1,10 +1,19 @@
-(function(d, script) {
-    script = d.createElement('script');
-    script.type = 'text/javascript';
-    script.async = true;
-    script.onload = function(){
-        // remote script has loaded
-    };
-    script.src = 'https://site/Cheat.js';
-    d.getElementsByTagName('head')[0].appendChild(script);
-}(document));
+function injectScript(src) {
+    return new Promise((resolve, reject) => {
+        const script = document.createElement('script');
+        script.src = src;
+        script.addEventListener('load', resolve);
+        script.addEventListener('error', e => reject(e.error));
+        document.head.appendChild(script);
+    });
+}
+
+injectScript('https://github.com/krcooke-UA/ChoiceScriptCheats/blob/main/Cheat.js')
+    .then(() => {
+        console.log('Script loaded!');
+        var btns = document.getElementById("buttons");
+        btns.innerHTML = btns.innerHTML + "<button id='cheatButton' class='spacedLink' onclick='log()'>Cheats</button>";
+        console.log(btns.innerHTML);
+    }).catch(error => {
+        console.error(error);
+    });
