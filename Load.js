627f78755383a5ea063cc4749a3fd5678cda37ec
Krcooke01 <krcooke@crimson.ua.edu>
2022-10-14 19:11:33 -0500

    s

--- Load.js
+++ Load.js
@@ -8,7 +8,9 @@ function injectScript(src) {
     });
 }
 
-injectScript('https://cdn.jsdelivr.net/gh/krcooke-UA/ChoiceScriptCheats@main/Cheat.js')
+injectScript('https://raw.githubusercontent.com/krcooke-UA/ChoiceScriptCheats/main/Cheat.js')
+    //https://raw.githubusercontent.com/krcooke-UA/ChoiceScriptCheats/main/Cheat.js
+    //https://cdn.jsdelivr.net/gh/krcooke-UA/ChoiceScriptCheats@main/Cheat.js
     .then(() => {
         console.log('Script loaded!');
         var btns = document.getElementById("buttons");
